a1ced8477be0d604c0079941353eec043dd148d9
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-08 11:19:22 -0600

    Add addtional tests to intialize method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe Board do
     it "exists" do
       expect(@board).to be_a(Board)
       expect(@board.cells).to be_a(Hash)
+      expect(@board.cells["A1"]).to be_a(Cell)
+      expect(@board.cells["A1"].coordinate).to eq("A1")
+      expect(@board.cells.length).to eq(16)
     end
   end
 end
