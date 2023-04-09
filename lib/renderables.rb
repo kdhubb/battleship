785e912a384752e8d53ce75c8b0be785bb5208d0
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-09 11:43:31 -0600

    wip render module

--- lib/renderables.rb
+++ lib/renderables.rb
@@ -9,6 +9,7 @@ module Renderables
   end
   
   def render(param = false)
+  
     if param == false
       if !fired_upon?
         "."
@@ -33,4 +34,16 @@ module Renderables
       end
     end
   end
+end
+
+if self.is_a?(Hash)
+  illustrated = []
+  @cells.each_value do |cell|
+    illustrated << cell.render
+  end
+  return_array = []
+  illustrated.each_slice(4) do |line|
+    return_array << line
+  end
+  "  1 2 3 4 \nA #{return_array[0].join(" ")} \nB #{return_array[1].join(" ")} \nC #{return_array[2].join(" ")} \nD #{return_array[3].join(" ")} \n"
 end
