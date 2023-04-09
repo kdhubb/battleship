52a6f3320fd7f14fa61cf4abcc37507e31c41eef
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-09 12:36:39 -0600

    deleted commented code in renderable module

--- lib/renderables.rb
+++ lib/renderables.rb
@@ -34,15 +34,3 @@ module Renderables
     end
   end
 end
-
-# if self.is_a?(Hash)
-#   illustrated = []
-#   @cells.each_value do |cell|
-#     illustrated << cell.render
-#   end
-#   return_array = []
-#   illustrated.each_slice(4) do |line|
-#     return_array << line
-#   end
-#   "  1 2 3 4 \nA #{return_array[0].join(" ")} \nB #{return_array[1].join(" ")} \nC #{return_array[2].join(" ")} \nD #{return_array[3].join(" ")} \n"
-# end
