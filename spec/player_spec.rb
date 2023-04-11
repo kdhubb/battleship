485e32f7d28423fd06d148039cb2de13450fbb03
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-10 19:49:37 -0600

    Add player with ships, board and ability to place ships

---
+++ spec/player_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+RSpec.describe Player do
+  before(:each) do 
+    @user = Player.new
+    @board = Board.new
+    # @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  describe "initialize" do
+    it "exists" do
+      expect(@user).to be_a(Player)
+      expect(@user.board).to be_a(Board)
+      
+      @user.board.place(@user.cruiser, ["A1", "A2", "A3"])
+      
+      expect(@user.board.cells["A1"].ship).to eq(@user.cruiser)
+    end
+  end
+end
