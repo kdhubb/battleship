eb622044cb827bb677fd61fbf8efecfc77aeee05
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-12 16:57:07 -0600

    Add begin firing method to game

--- lib/game.rb
+++ lib/game.rb
@@ -4,6 +4,7 @@ attr_reader :player,
   def initialize
     @player = nil
     @computer = nil
+    @turn = nil
   end
 
   def new_players
@@ -38,7 +39,7 @@ attr_reader :player,
   
   def player_cruiser_placement
     puts "Enter the squares for the Cruiser (3 spaces):"
-    raw_input = gets.split(" ")
+    raw_input = gets.upcase.split(" ")
     if @player.board.valid_placement?(@player.cruiser, raw_input)
       @player.board.place(@player.cruiser, raw_input)
       puts @player.board.render(true)
@@ -52,7 +53,7 @@ attr_reader :player,
   
   def player_submarine_placement
     puts "Enter the squares for the Submarine (2 spaces):"
-    raw_input = gets.split(" ")
+    raw_input = gets.upcase.split(" ")
     if @player.board.valid_placement?(@player.submarine, raw_input)
       @player.board.place(@player.submarine, raw_input)
       puts @player.board.render(true)
@@ -65,7 +66,12 @@ attr_reader :player,
   end
 
   def begin_firing
-
+    @turn = Turn.new
+    puts @turn.render_boards(@computer, @player)
+    puts "Enter the coordinate for your shot:"
+    shot_input = gets.chomp.upcase
+    @turn.computer_fire(@computer, @player)
+    @turn.player_fire(@player, @computer, shot_input)
   end
 
   def winner
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,6 @@ require "spec_helper"
 RSpec.describe Game do 
   before(:each) do 
     @game = Game.new
-    # @player = Player.new
-    # @computer = Computer.new
   end
   
   describe "initialize" do 
@@ -25,7 +23,6 @@ RSpec.describe Game do
     end
   end
   
-  
   ## Need some serious work on tests with gets
   describe "gets" do
     context "start" do
@@ -35,20 +32,19 @@ RSpec.describe Game do
         expect(@game.computer.board.cells).to be_a(Hash)
       end
     end
-  end
-  
-    context "setup" do
-      xit "allows the player to enter their cruiser" do
-        @game.setup
 
-        expect(@game.computer.board.cells).to be_a(Hash)
-      end
-    end
-  
     context "player_cruiser_placement" do
       xit "takes player input and warns of invalid coordinate" do
         @game.setup
         @game.player_cruiser_placement
+  
+        expect(@game.computer.board.cells).to be_a(Hash)
+      end
+    end
+  
+    context "setup" do
+      xit "allows the player to enter their cruiser" do
+        @game.setup
 
         expect(@game.computer.board.cells).to be_a(Hash)
       end
