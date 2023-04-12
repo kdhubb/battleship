d43e804f3d1a38ca6dd01941be1a7ce52d96f643
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-11 19:04:13 -0600

    adds turn class initialize and test

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+class Turn
+  attr_reader :player,
+              :macbook
+  def initialize(player, macbook)
+    @player = player
+    @macbook = macbook
+  end
+
+def setup 
+end
+
+def basic
+end
+
+def winner
+end
+
+end
---
+++ spec/turn_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+RSpec.describe Turn do 
+  before(:each) do
+    @turn = Turn.new
+  end
+
+  describe "initialize" do 
+    it "exists" do 
+      expect(@turn).to be_a(Turn)
+      expect(@turn.player).to be_a(Player)
+      expect(@turn.macbook).to be_a(Computer)
+    end
+  end
+end
