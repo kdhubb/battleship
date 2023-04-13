3d98d159a912d625722ef3919b410d1cacd50ba9
Garrett Gregor <garrett.gregor@gmail.com>
2023-04-12 18:20:38 -0600

    Finish reformatting turn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,8 @@
 class Turn
   def render_boards(computer, player, param = false)
-    "\n============COMPUTER BOARD============ \n" +
+    "\n ============COMPUTER BOARD============ \n" +
     computer.board.render(true) +
-    "\n=============PLAYER BOARD============= \n" +
+    "\n =============PLAYER BOARD============= \n" +
     player.board.render(true)
   end
 
@@ -15,12 +15,12 @@ class Turn
         computer.board.cells[input].fire_upon
         player_result = computer.board.cells[input].render_result
         computer_result = player.board.cells[computer.cells_fired_upon.last].render_result
-        puts "\nYour shot on #{input} #{player_result}. \n" +
-        "\nMy shot on #{computer.cells_fired_upon.last} #{computer_result}. \n"
+        puts "\n Your shot on #{input} #{player_result}. \n" +
+        "\n My shot on #{computer.cells_fired_upon.last} #{computer_result}. \n"
     elsif !computer.board.valid_coordinate?(input)
-      puts "\nYou're off the charts! Please enter a valid coordinate that is on the board:\n"
+      puts "\n You're off the charts! Please enter a valid coordinate that is on the board: \n"
     elsif computer.board.cells[input].fired_upon?
-      puts "\nYou already shot there! Try again:\n"
+      puts "\n You already shot there! Try again: \n"
     end
   end
 end
